f9ad74ad160171d5ce4ad9b300f26ffed48cae9b
FranciscoMColino <francisco.m.colino@gmail.com>
2021-05-05 10:24:09 +0100

    Producer thread runs task

--- src/thread.c
+++ src/thread.c
@@ -1,5 +1,10 @@
+#include "./lib.h"
 #include "./utils.h"
 
 void* thread_entry(void *arg){
     message_t recv_message = *(message_t*)arg;
+
+    int res = task(recv_message.tskload);
+    recv_message.tskres = res;
+
 }
